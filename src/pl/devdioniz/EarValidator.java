36c27b27639a91d9f947a34b41dd3705f583155e
denisu <denisszczukocki@o2.pl>
2017-03-29 22:58:14 +0200

    added policy validation function, small adjustment, still awful code - needs refactorization

--- src/pl/devdioniz/EarValidator.java
+++ src/pl/devdioniz/EarValidator.java
@@ -1,43 +1,161 @@
 package pl.devdioniz;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class EarValidator {
 
-	// test ear file under: https://publib.boulder.ibm.com/bpcsamp/gettingStarted/helloWorld/download.html
+	// test ear file under:
+	// https://publib.boulder.ibm.com/bpcsamp/gettingStarted/helloWorld/download.html
 	private static String INPUT_ZIP_FILE = "C:\\Users\\Desz\\Downloads\\HelloWorldApp.ear";
-	private static String OUTPUT_DIR = "H:\\temp\\HelloWorldApp";
+	private static String OUTPUT_DIR = "H:\\temp";
+	private static String APPROVED_WAS_POLICY = "H:\\temp\\approved.policy";
+
+	private static double expectedAppVersion;
 
 	private final static byte[] buffer = new byte[1024];
 
 	public static void main(String[] args) {
 
 		double start = System.currentTimeMillis();
-		
+
+		if (args.length == 1) {
+			try {
+				expectedAppVersion = Double.parseDouble(args[0]);
+			} catch (NumberFormatException e) {
+				System.out.println("Wrong version argument, cannot parse to double value");
+				System.exit(1);
+			}
+		} else {
+			expectedAppVersion = 2.0;
+		}
+
 		String[] appNameTab = INPUT_ZIP_FILE.split(Pattern.quote("\\"));
-		System.out.println(retrieveApplicationName(appNameTab));
+		OUTPUT_DIR = OUTPUT_DIR + File.separator + retrieveApplicationName(appNameTab);
 
-		//unzipIt(INPUT_ZIP_FILE, OUTPUT_DIR, true);
+		unzipIt(INPUT_ZIP_FILE, OUTPUT_DIR, true);
 
+		Path wasPolicyPath = findFile(OUTPUT_DIR, "application.xml");
+		System.out.println("Found was policy in: " + wasPolicyPath.toAbsolutePath());
+		
+		boolean isPolicyOK = false;
+		
+		if (wasPolicyPath != null) {
+			isPolicyOK = compareFiles(wasPolicyPath, APPROVED_WAS_POLICY);
+		}
+		
+		System.out.println("Policy OK: " + isPolicyOK);
+
+		Path versionXmlPath = findFile(OUTPUT_DIR, "web.xml");
+		System.out.println("Found version.xml in: " + versionXmlPath.toAbsolutePath());
+
+		if (versionXmlPath != null) {
+			validateAppVersion(versionXmlPath, expectedAppVersion);
+		}
+		
 		double end = System.currentTimeMillis();
 
-		System.out.println("Time taken: " + ((end - start) / 60) + " seconds.");
+		double totalTime = ((end - start) / 60);
+		System.out.println("Time taken: " + totalTime + " seconds.");
 
 	}
-	
+
+	private static boolean validateAppVersion(Path versionXmlPath, double expectedAppVersion2) {
+
+		File versionFile = new File(versionXmlPath.toUri());
+		try {
+			Files.readAllLines(versionXmlPath);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return true;
+	}
+
+	private static boolean compareFiles(Path wasPolicyPath, String approvedWasPolicy) {
+
+		try {
+			BufferedReader notApproved = new BufferedReader(new FileReader(new File(wasPolicyPath.toUri())));
+			BufferedReader approved = new BufferedReader(new FileReader(new File(approvedWasPolicy)));
+
+			List<String> approvedFile = new ArrayList<>();
+			String line = "";
+
+			while ((line = approved.readLine()) != null) {
+
+				approvedFile.add(line.trim());
+			}
+
+			while ((line = notApproved.readLine()) != null) {
+				line = line.trim();
+				if (!approvedFile.contains(line)) {
+					System.out.println("Difference in line:");
+					System.out.println(line);
+					return false;
+				}
+			}
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return true;
+
+	}
+
+	private static Path findFile(String sourceDir, String fileName) {
+
+		Path start = Paths.get(sourceDir);
+		int maxDepth = 5;
+		Path searchedFilePlace = null;
+
+		try {
+			Stream<Path> find = Files.find(start, maxDepth, (p, f) -> {
+				String tempFile = p.getFileName().toString();
+				return fileName.equals(tempFile);
+			});
+
+			// awful way of looking for exact path, TODO refactor it
+			Iterator<Path> iterator = find.iterator();
+			while (iterator.hasNext()) {
+
+				Path next = iterator.next();
+				searchedFilePlace = next;
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return searchedFilePlace;
+	}
+
 	private static String retrieveApplicationName(String[] splittedZipPath) {
-		return splittedZipPath[splittedZipPath.length - 1].substring(0, splittedZipPath[splittedZipPath.length - 1].length() - 4);
+		int lastArrayIndex = splittedZipPath.length - 1;
+		return splittedZipPath[lastArrayIndex].substring(0, splittedZipPath[lastArrayIndex].length() - 4);
 	}
 
-	private static void unzipIt(final String inputZip, final String outputDir, boolean recursively) {
+	private static boolean unzipIt(final String inputZip, final String outputDir, boolean recursively) {
 
 		// make sure input zip exists
 		File input = new File(inputZip);
@@ -69,7 +187,7 @@ public class EarValidator {
 				// create parent directories to avoid FileNotFoundException
 				new File(file.getParent()).mkdirs();
 
-				System.out.println(file);
+				// System.out.println(file);
 
 				if (entry.isDirectory()) {
 					continue;
@@ -95,12 +213,16 @@ public class EarValidator {
 				}
 			}
 
+			return true;
+
 		} catch (FileNotFoundException e) {
 			System.out.println("Cannot find file.");
 			e.printStackTrace();
+			return false;
 		} catch (IOException e) {
 			System.out.println("Cannot perform IO operation.");
 			e.printStackTrace();
+			return false;
 		}
 
 		// open destination output directory in Windows explorer
